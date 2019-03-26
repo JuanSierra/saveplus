245ca3d9f120bb74e2448929b2a0e3d61643bb42
Juan Sierra <jksierra@gmail.com>
2019-03-26 12:12:36 +0000

    Main exception scenarios handled. Json export with byte data for images

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const p_autoscroll = require('puppeteer-autoscroll-down');
 const scrollPageToBottom = p_autoscroll.scrollPageToBottom;
 const fs = require('fs');
 const htmlToText = require('html-to-text');
+var request = require('request');
 
 dotenv.config({path: 'dot.env'});
 
@@ -30,16 +31,23 @@ const clickByText = async (page, text) => {
   }
 };
 
+function downloadImage(url) {
+	return new Promise((resolve, reject) => {
+			request(url, (error, response, body) => {
+					if (error) reject(error);
+					if (response.statusCode != 200) {
+							reject('Invalid status code <' + response.statusCode + '>');
+					}
+		var encoded = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
+					resolve(encoded);
+			});
+	});
+}
+
 function delay(timeout) {
   return new Promise((resolve) => {
     setTimeout(resolve, timeout);
   });
-}
-
-Array.prototype.forEachAsync = async function(cb){
-	for(let x of this){
-		await cb(x);
-	}
 }
 
 async function scrapeInfiniteScrollItems(
@@ -52,7 +60,7 @@ async function scrapeInfiniteScrollItems(
     let previousHeight;
 	let controlText = await page.evaluate(() => document.querySelector('div[data-waitmessage]>div[role="heading"]').textContent);
 	
-    while (controlText!='Looks like you\'ve reached the end'){//items.length < itemTargetCount) {
+    while (controlText!='Looks like you\'ve reached the end'){
       previousHeight = await page.evaluate('document.body.scrollHeight');
       await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
       await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
@@ -69,7 +77,7 @@ async function scrapeInfiniteScrollItems(
     await page.setViewport({ width: 1280, height: 800 })
     await page.goto('https://plus.google.com')
     
-	await clickByText(page, `Sign in`);
+		await clickByText(page, `Sign in`);
     const navigationPromise = page.waitForNavigation()
     
     //page.waitForNavigation()
@@ -82,67 +90,87 @@ async function scrapeInfiniteScrollItems(
     
     await page.waitForSelector('#passwordNext', { visible: true })
     await page.click('#passwordNext')
-    
-	//navigationPromise
-	await page.waitForSelector('a[href="./settings"]')
-	await page.click('a[href="./settings"]')
-	
-	navigationPromise
-	await page.waitForSelector('a[href="./apps/activities"]')
-	await page.click('a[href="./apps/activities"]')
-	
-	navigationPromise
-	await page.goto('https://plus.google.com/apps/activities/plus_one_posts')
-	
-	//autoScroll(page);
-	await scrapeInfiniteScrollItems(page, 100);
-	let items = await page.$x(`//div[@role="listitem" and @tabindex=0]`);
-	let images = await page.$x(`//div[@role="listitem" and @tabindex=0]/div[1]/img`);
-	let contents = [];
-	await console.log(`ITEMS => ${items.length}`);
-	
-	for (let i=0; i<items.length; i++) {
-		let element = items[i];
-		await console.log(i);
-		
-		let valueHandle = await images[i].getProperty('src');
-		let linkText = await valueHandle.jsonValue();
 
-		await console.log(linkText);
-		if(linkText == "")
-			continue;
-		
-		await element.click();
-		
-		try{
-			await page.waitForSelector('div[data-cai="undefined"]');
+		await page.waitForSelector('a[href="./settings"]')
+		await delay(500);
+		await page.click('a[href="./settings"]')
 			
-			let content = await page.$eval('div[data-cai="undefined"]', (el) => { return el.innerHTML});
-			let data2 = await htmlToText.fromString(content);
-			await console.log(data2);
-			await contents.push(data2);
-			await delay(500);
-			await page.goBack();
-			await page.waitFor(() => !document.querySelector('div[data-cai="undefined"]'));
-		}catch(err){
-			await page.goBack();
-			await page.waitFor(() => !document.querySelector('div[data-cai="undefined"]'));
-			await console.log('CAN DETECT');
-			await scrapeInfiniteScrollItems(page, 100);
-			items = await page.$x(`//div[@role="listitem" and @tabindex=0]`);
-			images = await page.$x(`//div[@role="listitem" and @tabindex=0]/div[1]/img`);
+		navigationPromise
+		await page.waitForSelector('a[href="./apps/activities"]')
+		await page.click('a[href="./apps/activities"]')
+		
+		navigationPromise
+		await page.goto('https://plus.google.com/apps/activities/plus_one_posts')
+		
+		//autoScroll(page);
+		await scrapeInfiniteScrollItems(page, 100);
+		let items = await page.$x(`//div[@role="listitem" and @tabindex=0]`);
+		let images = await page.$x(`//div[@role="listitem" and @tabindex=0]/div[1]/img`);
+		let contents = [];
+		await console.log(`ITEMS => ${items.length}`);
+		
+		for (let i=0; i<items.length; i++) {
+			let element = items[i];
+			await console.log(i);
+			
+			let valueHandle = await images[i].getProperty('src');
+			let linkText = await valueHandle.jsonValue();
 
-			continue;
+			// first error scenario being handled
+			await console.log(linkText);
+			if(linkText == "")
+				continue;
+			
+			await element.click();
+			
+			try{
+				await page.waitForSelector('div[data-cai="undefined"]');
+				let user_img = await page.$eval('div[data-ccsc] img', (el) => { return el.src});
+				var dataimg = await downloadImage(user_img)
+				console.log(dataimg);
+			
+				let user = await page.$eval('div[data-ccsc]>div a', (el) => { return el.innerText});
+				let circle = await page.$eval('div[data-ccsc]>div>div a:nth-child(2)', (el) => { return el.innerHTML});
+				let date = await page.$eval('div[data-ccsc]>div>a', (el) => { return el.innerText});
+				let content = await page.$eval('div[data-cai="undefined"]', (el) => { return el.innerHTML});
+				let data2 = await htmlToText.fromString(content);
+
+				let item = new PlusItem(user, dataimg, circle, date, data2)
+
+				await contents.push(item);
+				await delay(500);
+				await page.goBack();
+				await page.waitFor(() => !document.querySelector('div[data-cai="undefined"]'));
+			}catch(err){
+				// second error scenario being handled
+				await page.goBack();
+				await page.waitFor(() => !document.querySelector('div[data-cai="undefined"]'));
+				await scrapeInfiniteScrollItems(page, 100);
+				items = await page.$x(`//div[@role="listitem" and @tabindex=0]`);
+				images = await page.$x(`//div[@role="listitem" and @tabindex=0]/div[1]/img`);
+
+				continue;
+			}
 		}
-	}
 	
-	//await console.log(contents);
-	let x = await (async function() {
-		let file = fs.createWriteStream('extracted.txt');
-		file.on('error', function(err) { /* error handling */ });
-		contents.forEach(function(v) { file.write(v + '\n'); });
-		file.end();
-	})();
+		//await console.log(contents);
+		let x = await (async function() {
+			let file = fs.writeFileSync('extracted.json', JSON.stringify(contents));
+			//contents.forEach(function(v) { file.write(v + '\n'); });
+			//file.write();
+			//file.end();
+		})();
 	
   //await browser.close()
 })()
+
+
+class PlusItem {
+  constructor(account, avatar, circle, date, content) {
+		this.account = account;
+		this.avatar = avatar;
+		this.circle = circle;
+		this.date = date;
+		this.content = content;
+  }
+}
